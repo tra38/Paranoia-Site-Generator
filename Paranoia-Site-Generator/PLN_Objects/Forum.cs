0b877c2b0f08fc9ecbc79fc410f66d423b4a1db5
tra38 <tra38@nau.edu>
2020-04-10 23:39:50 -0500

    Initial commit, rough draft with proof of concept for extracting data from database and notes on how to proceed next

---
+++ Paranoia-Site-Generator/PLN_Objects/Forum.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using Dapper;
+using System;
+using System.Reflection;
+
+namespace Paranoia_Site_Generator
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class SqlStatement : Attribute
+    {
+        public string Statement { get; set; }
+
+        public SqlStatement(string statement)
+            => Statement = statement;
+    }
+
+    [SqlStatement("SELECT * FROM phpbb_forums" )]
+    public class Forum
+    {
+        public int ForumId;
+        public int CatId;
+        public string ForumName;
+        public string ForumDesc;
+        public int ForumStats;
+        public int ForumOrder;
+        public int ForumPosts;
+        public int ForumTopics;
+        public int ForumLastPostId;
+        public int PruneNext;
+        public int PruneEnable;
+        public int AuthView;
+        public int AuthRead;
+        public int AuthPost;
+        public int AuthReply;
+        public int AuthEdit;
+        public int AuthDelete;
+        public int AuthSticky;
+        public int AuthAnnounce;
+        public int AuthVote;
+        public int AuthPollCreate;
+        public int AuthAttachments;
+    }
+
+    public static class Factory
+    {
+        public static string ConnectionString           = "server=localhost;user=root;database=alphacomplex;port=3306;";
+
+        public static IEnumerable<T> Build<T>()
+            => Build<T>(GetSQLStatement<T>());
+
+        public static IEnumerable<T> Build<T>( string sqlStatement )
+        {
+            DefaultTypeMap.MatchNamesWithUnderscores = true;
+
+            using (var conn = new MySqlConnection(ConnectionString))
+            {
+                return conn.Query<T>(sqlStatement);
+            }
+        }
+
+        public static string GetSQLStatement<T>( )
+        {
+            var attribute = typeof(T).GetCustomAttribute<SqlStatement>(true);
+
+            if (attribute != null)
+                return attribute.Statement;
+            else
+                throw new Exception($"SQL Statement not found for { typeof(T).Name }");
+        }
+    }
+}
---
+++ Paranoia-Site-Generator/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Paranoia_Site_Generator
+{
+    class Program
+    {
+        /*
+        static public List<string> GetList(MySqlDataReader mySqlDataReader)
+        {
+            var list = new List<string>();
+
+            DataTable dt = new DataTable();
+            dt.Load(MySql);
+
+
+            var counter = mySqlDataReader.
+
+            foreach (var item in mySqlDataReader )
+            {
+                Console.WriteLine(item.ToString());
+                list.Add( item.ToString( ) );
+            }
+
+            return list;
+        }
+        */
+
+            //Stories:
+            //1) Gather data.
+            //2) Populate static site with data.
+
+            //Should the data simply be transformed into txt files? And then
+            //I transfer said txt files to a static site generator?
+            //Or do I roll my own static site generator?
+
+            //Seems there's too many specific limits, and I need the practice,
+            //so I'll generate my own static site generator. Time to look
+            //at the pattern book.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //var list = ForumFactory.Build();
+
+            var list = Factory.Build<Forum>();
+
+            foreach (var element in list)
+            {
+                Console.WriteLine($"{element.ForumName} - { element.ForumDesc }; topics: { element.ForumTopics }");
+            }
+
+            //string connStr = "server=localhost;user=root;database=alphacomplex;port=3306;";
+            //MySqlConnection conn = new MySqlConnection(connStr);
+            //try
+            //{
+            //    Console.WriteLine("Connecting to MySQL...");
+            //    conn.Open();
+
+            //    string sql = "SELECT * FROM phpbb_forums";
+            //    MySqlCommand cmd = new MySqlCommand(sql, conn);
+            //    MySqlDataReader rdr = cmd.ExecuteReader();
+
+            //    DataTable dt = new DataTable();
+
+            //    dt.Load(rdr);
+            //    foreach (DataColumn col in dt.Columns)
+            //    {
+            //        Console.WriteLine($"{col.ColumnName} Info:");
+
+            //        var counter = dt.Rows.Count;
+            //        for (int i = 1; i < counter; i++)
+            //        {
+            //            Console.WriteLine($"{col.ColumnName}:{dt.Rows[i][col]}");
+            //        }
+            //        //TODO: create a static site using 'col.ColumnName', displaying all forums' info
+
+            //        /*
+            //        foreach (DataRowCollection row in dt.Rows[0])
+            //        {
+
+            //            Console.WriteLine(row[col.ColumnName]);
+            //        }*/
+            //    }
+
+            //    /*
+
+            //    while (rdr.Read( ) )
+            //    {
+            //        var list = GetList(rdr);
+            //        Console.WriteLine(rdr[0]);
+            //        Console.WriteLine(rdr[1]);
+            //        Console.WriteLine(rdr[2]);
+            //        Console.WriteLine(string.Join( "---", GetList( rdr ) ) );
+            //    }
+            //    */
+            //    rdr.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.ToString());
+            //}
+
+            //conn.Close();
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+}
