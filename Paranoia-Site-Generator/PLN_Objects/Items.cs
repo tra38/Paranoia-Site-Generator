8bb3ec41c350d245d5e956d154a724130ca1a8fa
tra38 <tra38@nau.edu>
2020-04-12 12:21:25 -0500

    Add Items properties

--- Paranoia-Site-Generator/PLN_Objects/Items.cs
+++ Paranoia-Site-Generator/PLN_Objects/Items.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 namespace Paranoia_Site_Generator.PLN_Objects
 {
+    [SqlStatement("SELECT * FROM phpbb_ac_items")]
     public class Items
     {
-        public Items()
-        {
-        }
+        public int ItemId;
+        public string ItemName;
+        public int ItemCost;
+        public int ItemClearance;
+        public int ItemDecay;
+        public int ItemAction;
+        public string ItemDescription;
     }
 }
